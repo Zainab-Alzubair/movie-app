7c2bce9409fd3e90cc5b556eb074cf6c1042d4d3
Zainab-Alzubair <zainab.a.elzubair@gmail.com>
2023-01-10 13:11:13 +0300

    Add the Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import PageNotFound from './components/PageNotFound/PageNotFound';
+import MovieDetails from './components/MovieDetails/MovieDetails';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      app
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/movie/:imdbID" element={<MovieDetails />} />
+          <Route element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
