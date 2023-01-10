f8ce973c8734d149c5eb084415cc964ff639fb4e
Zainab-Alzubair <zainab.a.elzubair@gmail.com>
2023-01-10 14:56:55 +0300

    Fetch API

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import MovieListing from '../MovieListing/MovieListing';
+import APIKey from '../../common/apis/MovieApiKey';
+import movieApi from '../../common/apis/MovieApi';
 
-const Home = () => (
-  <div>
-    <div className="banner-img" />
-    <MovieListing />
-  </div>
-);
+const Home = () => {
+  useEffect(() => {
+    const movieText = 'Harry';
+    const fetchMovies = async () => {
+      const response = await movieApi
+        .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+        .catch((err) => {
+          console.log(err);
+        });
+      console.log(response);
+    };
+    fetchMovies();
+  }, []);
+  return (
+    <div>
+      <div className="banner-img" />
+      <MovieListing />
+    </div>
+  );
+};
 
 export default Home;
